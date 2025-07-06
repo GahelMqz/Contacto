b25f446a07297fece8b036b510db7a92794f61ca
GahelMqz <gaelaldairocho@gmail.com>
2025-07-06 02:47:21 -0500

    modify backend

--- server/index.js
+++ server/index.js
@@ -8,11 +8,11 @@ const jwt = require('jsonwebtoken')
 const verifyToken = require('../server/middleware/verifyToken')
 const router = express.Router()
 
-app.use(express.static('dist'))
+// app.use(express.static('dist'))
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+// })
 
 const app = express()
 const port = process.env.PORT || 3001
